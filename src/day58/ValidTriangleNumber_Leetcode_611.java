25aeb48e3e88d3c7a0168641e55b2be3998356c9
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-11-27 10:08:55 +0530

    Day 58 -> Valid Triangle Number Done

---
+++ src/day58/ValidTriangleNumber_Leetcode_611.java
@@ -0,0 +1,31 @@
+package day58;
+
+import java.util.Arrays;
+
+public class ValidTriangleNumber_Leetcode_611 {
+
+    //??
+    public static int triangleNumber(int[] nums) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int left = 0, right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    count += right - left;
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(triangleNumber(new int[]{2, 2, 3, 4}));
+    }
+}
