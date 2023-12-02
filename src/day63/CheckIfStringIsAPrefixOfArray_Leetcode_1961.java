01e9f7f411f2b7eb00d2a072a79c3675c298b95a
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-12-02 10:48:29 +0530

    Day 63 -> Check If String Is a Prefix of Array Done

---
+++ src/day63/CheckIfStringIsAPrefixOfArray_Leetcode_1961.java
@@ -0,0 +1,22 @@
+package day63;
+
+public class CheckIfStringIsAPrefixOfArray_Leetcode_1961 {
+
+    //??
+    public static boolean isPrefixString(String s, String[] words) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String str : words) {
+            stringBuilder.append(str);
+            if (stringBuilder.toString().startsWith(s) && s.length() >= stringBuilder.toString().length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrefixString("a", new String[]{"aa,aaa"}));
+    }
+}
