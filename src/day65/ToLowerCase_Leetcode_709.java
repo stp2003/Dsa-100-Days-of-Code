20ebfa74aa7e5ba2d0d9c0c0b77e1a9906d2bb16
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-12-04 10:27:27 +0530

    Day 65 -> To Lower Case Done

---
+++ src/day65/ToLowerCase_Leetcode_709.java
@@ -0,0 +1,14 @@
+package day65;
+
+public class ToLowerCase_Leetcode_709 {
+
+    //??
+    public static String toLowerCase(String s) {
+
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("LeetCode"));
+    }
+}
