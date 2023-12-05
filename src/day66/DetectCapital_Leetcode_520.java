53b24c3ebd0765f77cddfd939ac616d2321d7ee8
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-12-05 12:06:44 +0530

    Day 66 -> Detect Capital Done

---
+++ src/day66/DetectCapital_Leetcode_520.java
@@ -0,0 +1,42 @@
+package day66;
+
+public class DetectCapital_Leetcode_520 {
+
+    //??
+    public static boolean detectCapitalUse(String word) {
+
+        if (word.length() <= 1)
+            return true;
+
+        int a = word.charAt(0) - 'A';
+
+        //** if 1st char is lowercase, all chars should be lowercase
+        if (a >= 32) {
+            for (int i = 1; i < word.length(); i++) {
+                if (word.charAt(i) - 'A' < 32) return false;
+            }
+        }
+
+        //** if 1st char is uppercase
+        else {
+            //** if 2nd char is uppercase, all char should be uppercase
+            if (word.charAt(1) - 'A' < 32) {
+                for (int i = 2; i < word.length(); i++) {
+                    if (word.charAt(i) - 'A' >= 32) return false;
+                }
+            }
+
+            //** if 2nd is lowercase, then all char should be lowercase
+            else {
+                for (int i = 1; i < word.length(); i++) {
+                    if (word.charAt(i) - 'A' < 32) return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(detectCapitalUse("USA"));
+    }
+}
