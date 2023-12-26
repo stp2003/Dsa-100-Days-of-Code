69e63e1e8b4495b4de3d31c49c220e49117671dc
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-12-26 09:58:12 +0530

    Day 86 -> Complex Number Multiplication Done

---
+++ src/day87/CountBinarySubstrings_Leetcode_696.java
@@ -0,0 +1,30 @@
+package day87;
+
+public class CountBinarySubstrings_Leetcode_696 {
+
+    //??
+    public static int countBinarySubstrings(String s) {
+
+        int count = 1;
+        int flag = 0;
+        int ans = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                ans += Math.min(flag, count);
+                flag = count;
+                count = 1;
+            }
+        }
+
+        ans += Math.min(flag, count);
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countBinarySubstrings("00110011"));
+    }
+}
