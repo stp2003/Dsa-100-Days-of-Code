95aca9bfc69cefea623214ac90025796a7f8d786
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-01-03 11:11:45 +0530

    Day 95 -> Number of Laser Beams in a Bank Done

---
+++ src/day95/NumberOfLaserBeamsInABank_Leetcode_2125.java
@@ -0,0 +1,35 @@
+package day95;
+
+public class NumberOfLaserBeamsInABank_Leetcode_2125 {
+    //??
+    public static int numberOfBeams(String[] bank) {
+        int flag = 0;
+        int ans = 0;
+
+        for (String s : bank) {
+            int currentCount = countOnes(s);
+
+            if (currentCount > 0) {
+                ans += flag * currentCount;
+                flag = currentCount;
+            }
+        }
+
+        return ans;
+    }
+
+
+    public static int countOnes(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfBeams(new String[]{"011001", "000000", "010100", "001000"}));
+    }
+}
