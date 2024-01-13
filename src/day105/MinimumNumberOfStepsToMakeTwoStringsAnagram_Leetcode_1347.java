7ec2fe30b8022af81de27911634ab49778bd4f94
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-01-13 09:46:40 +0530

    Day 105 -> Minimum Number of Steps to Make Two Strings Anagram Done

---
+++ src/day105/MinimumNumberOfStepsToMakeTwoStringsAnagram_Leetcode_1347.java
@@ -0,0 +1,32 @@
+package day105;
+
+public class MinimumNumberOfStepsToMakeTwoStringsAnagram_Leetcode_1347 {
+
+    //??
+    public static int minSteps(String s, String t) {
+
+        //** The indices 0 to 25 correspond to the characters 'a' to 'z' ->
+        int[] sCount = new int[26];
+        int[] tCount = new int[26];
+
+        //?? when character found the count is incremented ->
+        for (char ch : s.toCharArray()) {
+            sCount[ch - 'a']++;
+        }
+
+        for (char ch : t.toCharArray()) {
+            tCount[ch - 'a']++;
+        }
+
+        int totalSteps = 0;
+        for (int i = 0; i < 26; i++) {
+            totalSteps += Math.abs(sCount[i] - tCount[i]);
+        }
+
+        return totalSteps / 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minSteps("bba", "aba"));
+    }
+}
