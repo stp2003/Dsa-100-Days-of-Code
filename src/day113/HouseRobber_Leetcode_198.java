5356fa027458b57e94f0822b7e8bf058ae8d16d9
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-01-26 15:31:27 +0530

    Day 113 -> House Robber Done

---
+++ src/day113/HouseRobber_Leetcode_198.java
@@ -0,0 +1,22 @@
+package day113;
+
+public class HouseRobber_Leetcode_198 {
+
+    //??
+    public static int rob(int[] nums) {
+
+        int[] dp = new int[nums.length + 1];
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp[dp.length - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{1, 2, 3, 6, 5}));
+    }
+}
