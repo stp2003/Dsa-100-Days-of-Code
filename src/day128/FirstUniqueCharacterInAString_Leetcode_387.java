1f75c8bd243213c8cb4f8e773ac17a3dba1365b7
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-02-05 12:31:44 +0530

    Day 128 -> First Unique Character in a String Done

---
+++ src/day128/FirstUniqueCharacterInAString_Leetcode_387.java
@@ -0,0 +1,24 @@
+package day128;
+
+public class FirstUniqueCharacterInAString_Leetcode_387 {
+
+    //??
+    public static int firstUniqChar(String s) {
+
+        int ans = -1;
+
+        for (char ch : s.toCharArray()) {
+
+            if (s.indexOf(ch) == s.lastIndexOf(ch)) {
+                return s.indexOf(ch);
+            }
+        }
+
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("acf"));
+    }
+}
