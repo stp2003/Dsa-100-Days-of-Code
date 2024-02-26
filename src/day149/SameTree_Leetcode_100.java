ada54fcab9853104c4d69e3ec42e1b7b49f590b2
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-02-26 21:10:48 +0530

    Day 149 -> Same Tree Done

---
+++ src/day149/SameTree_Leetcode_100.java
@@ -0,0 +1,39 @@
+package day149;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SameTree_Leetcode_100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null)
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }
+
+}
